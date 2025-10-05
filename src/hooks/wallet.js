048409e27fa18b7c394a019f3706ac153e77e0c0
Rawakinodes <59562911+rawakinode@users.noreply.github.com>
2025-10-05 13:13:56 +0800

    Update wallet.js

--- src/hooks/wallet.js
+++ src/hooks/wallet.js
@@ -5,7 +5,7 @@ import { monadTestnet } from 'viem/chains';
 
 
 export async function createMetaMaskWalletClient() {
-    if (!window.ethereum) throw new Error("MetaMask not found");
+    // if (!window.ethereum) throw new Error("MetaMask not found");
 
     const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
     const address = accounts[0];
